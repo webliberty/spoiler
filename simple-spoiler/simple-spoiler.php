2ee5f3c19e5f045748ca9e4204c387ce17fc89dc
webliberty <webliberty@yandex.ru>
2018-01-08 22:47:29 +0300

    Update simple-spoiler.php

--- simple-spoiler/simple-spoiler.php
+++ simple-spoiler/simple-spoiler.php
@@ -15,24 +15,26 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-function simple-spoiler($atts, $content) {
-	if (!isset($atts[name])) {$sp_name = __('Spoiler', 'simple-spoiler');}
-	else {$sp_name = $atts[name];}
+function s_spoiler($atts, $content) {
+	if ( ! isset($atts[name]) ) {
+		$sp_name = __( 'Spoiler', 'simple-spoiler' );
+	} else {
+		$sp_name = $atts[name];
+	}
 	return '<div class="spoiler-wrap">
-		<div class="spoiler-head folded">'.$sp_name.'</div>
-		<div class="spoiler-body">'.$content.'</div>
-	</div>';
+					<div class="spoiler-head folded">'.$sp_name.'</div>
+				<div class="spoiler-body">'.$content.'</div>
+			</div>';
 }
-add_shortcode('spoiler', 'simple-spoiler');
+add_shortcode( 'spoiler', 's_spoiler' );
 
-add_action( 'wp_enqueue_scripts', 'is_styles_scripts' );
-function is_styles_scripts() {
+add_action( 'wp_enqueue_scripts', 'do_s_spoiler' );
+function do_s_spoiler() {
 	global $post;
-	wp_register_style( 'simple-spoiler_style', plugins_url( 'css/simple-spoiler.css', __FILE__ ), null, '1.0' );
-	wp_register_script( 'simple-spoiler_script', plugins_url( 'js/simple-spoiler.js', __FILE__ ), array( 'jquery' ), '1.0', true );
-
+	wp_register_style( 's_spoiler_style', plugins_url( 'css/simple-spoiler.css', __FILE__ ), null, '1.0' );
+	wp_register_script( 's_spoiler_script', plugins_url( 'js/simple-spoiler.js', __FILE__ ), array( 'jquery' ), '1.0', true );
 	if ( has_shortcode( $post->post_content, 'spoiler' ) ) {
-		wp_enqueue_style( 'simple-spoiler_style' );
-		wp_enqueue_script( 'simple-spoiler_script' );
+		wp_enqueue_style( 's_spoiler_style' );
+		wp_enqueue_script( 's_spoiler_script' );
 	}
 }
