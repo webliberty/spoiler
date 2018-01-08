9c7dc6a1fbb700ae7e84012bd6dd3f6af022a885
webliberty <webliberty@yandex.ru>
2018-01-08 22:47:07 +0300

    Update simple-spoiler.js

--- simple-spoiler/js/simple-spoiler.js
+++ simple-spoiler/js/simple-spoiler.js
@@ -1,6 +1,6 @@
-jQuery(document).ready(function(){
-	jQuery('.spoiler-body').hide()
-	jQuery('.spoiler-head').click(function(){
-		jQuery(this).toggleClass("folded").toggleClass("unfolded").next().toggle()
-	})
-})
+jQuery(document).ready(function() {
+	jQuery('.spoiler-body').hide();
+	jQuery('.spoiler-head').click(function() {
+		jQuery(this).toggleClass('folded').toggleClass('unfolded').next().toggle();
+	});
+});
